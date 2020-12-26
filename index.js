2cc342a10b861c0ebc7225cb13fc6d41387fc2c0
timlin <timlin1972@gmail.com>
2020-12-26 22:28:00 +0800

    i18n refactor

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const DEF_PORT = 3000;
 const DEF_LEVEL = 'info';
 const DEF_LOGGER = null;
 const DEF_PUBLIC_DIR = null;
+const DEF_I18N = null;
 
 const DEF_CONFIGS = {
   logger: DEF_LOGGER,
   port: DEF_PORT,
   publicDir: DEF_PUBLIC_DIR,
+  i18n: DEF_I18N,
 }
 
 class WebServer {
@@ -19,6 +21,7 @@ class WebServer {
     this.port = configs.port || DEF_PORT;
     this.logger = configs.logger || DEF_LOGGER;
     this.publicDir = configs.publicDir || DEF_PUBLIC_DIR;
+    this.i18n = configs.i18n || DEF_I18N;
 
     this.httpServer = null;
 
@@ -29,7 +32,11 @@ class WebServer {
 
   start = () => new Promise((resolve) => {
       this.httpServer = this.app.listen(this.port, () => {
-        this.log('info', `http server is listening at port ${this.port}`);
+        const msgListeningI18n = this.i18n 
+            ? this.i18n.t('http server is listening at port') 
+            : 'http server is listening at port';
+
+        this.log('info', `${msgListeningI18n} ${this.port}`);
         resolve(null);
         return;
       });
@@ -44,10 +51,12 @@ class WebServer {
 
   getApp = () => this.app;
 
-  log = (level=DEF_LEVEL, msg) => 
+  log = (level=DEF_LEVEL, msg) => {
+    const msgI18n = this.i18n ? this.i18n.t(msg) : msg;
     this.logger ? 
-      this.logger.log(MODULE_NAME, level, msg) :
-      console.log(`${level}: [${MODULE_NAME}] ${msg}`);
+      this.logger.log(MODULE_NAME, level, msgI18n) :
+      console.log(`${level}: [${MODULE_NAME}] ${msgI18n}`);
+  }
 
   toString = () => `[${MODULE_NAME}]\n\
     \tlogger: ${this.logger ? 'yes' : 'no'}\n\
