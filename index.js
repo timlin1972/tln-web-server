8ec17672f4bd559c44db9f5c8816b9926ac046eb
timlin <timlin1972@gmail.com>
2020-12-26 21:15:47 +0800

    Refactor

--- index.js
+++ index.js
@@ -27,47 +27,33 @@ class WebServer {
     this.log('info', 'Initialized');
   }
 
-  start() {
-    return new Promise((resolve) => {
+  start = () => new Promise((resolve) => {
       this.httpServer = this.app.listen(this.port, () => {
         this.log('info', `http server is listening at port ${this.port}`);
         resolve(null);
         return;
       });
     });
-  }
 
-  stop() {
-    return new Promise((resolve) => {
+  stop = () => new Promise((resolve) => {
       this.httpServer.close(() => {
         this.log('warn', 'http server is closed');
         resolve(null);
-        return;
       })
     });
-  }
 
-  getApp() {
-    return this.app;
-  }
+  getApp = () => this.app;
 
-  log = (level=DEF_LEVEL, msg) => {
-    if (this.logger !== null) {
-      this.logger.log(MODULE_NAME, level, msg)
-    }
-    else {
+  log = (level=DEF_LEVEL, msg) => 
+    this.logger ? 
+      this.logger.log(MODULE_NAME, level, msg) :
       console.log(`${level}: [${MODULE_NAME}] ${msg}`);
-    }
-  }
 
-  toString = () => {
-    return `[${MODULE_NAME}]\n \
-      \tlogger: ${this.logger ? 'yes' : 'no'}\n \
-      \tport: ${this.port}\n \
-      \tpublicDir: ${this.publicDir}\n \
-      `;
-  }
-  
+  toString = () => `[${MODULE_NAME}]\n\
+    \tlogger: ${this.logger ? 'yes' : 'no'}\n\
+    \tport: ${this.port}\n\
+    \tpublicDir: ${this.publicDir}\n\
+    `;  
 }
 
 module.exports = WebServer;
